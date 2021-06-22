75695fbf9fdf1959d02ff2966915a58630ed400f
Rodolfo Campos <rodolfo@strongdm.com>
2021-06-22 11:35:58 -0400

    Separate conf in different files

--- fluentd/start.sh
+++ fluentd/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-# Build custom fluent.conf
+BASE_DIR=/fluentd/etc
+INPUT=$BASE_DIR/input-rsyslog-csv.conf
+PROCESS=$BASE_DIR/process.conf
+OUTPUT=$BASE_DIR/output-s3.conf
 
-fluentd -c fluentd/etc/fluent.conf 
+cat $INPUT $PROCESS $OUTPUT > $BASE_DIR/fluent.conf
+fluentd -c $BASE_DIR/fluent.conf 
