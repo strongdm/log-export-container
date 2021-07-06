008f68325f24e09fd4fa88410ba46ec299a929f9
Rodolfo Campos <rodolfo@strongdm.com>
2021-07-06 07:51:47 -0400

    Support multiple storages

--- start.sh
+++ start.sh
@@ -2,12 +2,26 @@
 
 ETC_DIR=/fluentd/etc
 
-get_conf() {
+get_intput_conf_name() {
     echo $1 | awk '{ gsub(/ /,""); print tolower($0) }'
 }
 
-get_input_conf() {
-    conf=$(get_conf $LOG_EXPORT_CONTAINER_INPUT)
+get_output_conf_stores() {
+    for s in stdout s3 cloudwatch splunk-hec datadog; do
+        contains=$(echo $LOG_EXPORT_CONTAINER_OUTPUT | grep -wq $s; echo $?)
+        if [ $contains -eq 0 ]; then
+            export conf="$conf $s"
+        fi
+    done
+    if [ "$conf" == "" ]; then
+        echo "stdout"
+    else
+        echo $conf
+    fi
+}
+
+get_input_conf_file() {
+    conf=$(get_intput_conf_name $LOG_EXPORT_CONTAINER_INPUT)
     if [ "$conf" == "csv" ]; then
         echo $ETC_DIR/input-rsyslog-csv.conf
     else
@@ -15,8 +29,8 @@ get_input_conf() {
     fi
 }
 
-get_process_conf() {
-    conf=$(get_conf $LOG_EXPORT_CONTAINER_INPUT)
+get_process_conf_file() {
+    conf=$(get_intput_conf_name $LOG_EXPORT_CONTAINER_INPUT)
     if [ "$conf" == "csv" ]; then
         echo $ETC_DIR/process-csv.conf
     else
@@ -25,19 +39,16 @@ get_process_conf() {
 }
 
 get_output_conf() {
-    conf=$(get_conf $LOG_EXPORT_CONTAINER_OUTPUT)
-    if [ "$conf" == "s3" ]; then
-        echo $ETC_DIR/output-s3.conf
-    elif [ "$conf" == "cloudwatch" ]; then
-        echo $ETC_DIR/output-cloudwatch.conf
-    elif [ "$conf" == "splunk-hec" ]; then
-        echo $ETC_DIR/output-splunk-hec.conf
-    elif [ "$conf" == "datadog" ]; then
-        echo $ETC_DIR/output-datadog.conf
-    else
-        echo $ETC_DIR/output-stdout.conf
+  new_line=""
+  for s in $(get_output_conf_stores); do
+    export stores="$stores$new_line$(cat $ETC_DIR/output-$s.conf)"
+    if [ "$new_line" = "" ]; then
+      new_line=$'\n'
     fi
+  done
+  envsubst < $ETC_DIR/output-template.conf
 }
 
-cat $(get_input_conf) $(get_process_conf) $(get_output_conf) > $ETC_DIR/fluent.conf
+cat $(get_input_conf_file) $(get_process_conf_file) > $ETC_DIR/fluent.conf
+echo "$(get_output_conf)" >> $ETC_DIR/fluent.conf
 exec fluentd -c $ETC_DIR/fluent.conf -p /fluentd/plugins
