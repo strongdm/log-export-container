a28b06e4a0edb30643df6fc33f19aa8a0c3628fe
Lucas Santos <lrs20@aluno.ifal.edu.br>
2022-02-25 14:18:47 -0300

    Fix setup-ansible-and-lec.sh and lec-playbook.yml

--- setup-ansible-and-lec.sh
+++ setup-ansible-and-lec.sh
@@ -1,5 +1,5 @@
 apt update
-sudo apt install software-properties-common
+apt install --yes software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install --yes ansible
 ansible-playbook lec-playbook.yml
